b97283d5dacbaf41f41682fdc64e28d63176d506
Ian Greulich <ian@greulich.me>
2018-11-03 17:58:15 -0400

    rules object

---
+++ src/data/rules.js
@@ -0,0 +1,28 @@
+const rules = [
+  {
+    item: 'A',
+    unitPoints: '50',
+    bonusQty: '3',
+    bonus: '200',
+  },
+  {
+    item: 'B',
+    unitPoints: '30',
+    bonusQty: '2',
+    bonus: '90',
+  },
+  {
+    item: 'C',
+    unitPoints: '20',
+    bonusQty: undefined,
+    bonus: undefined,
+  },
+  {
+    item: 'D',
+    unitPoints: '15',
+    bonusQty: undefined,
+    bonus: undefined,
+  },
+];
+
+export default rules;
