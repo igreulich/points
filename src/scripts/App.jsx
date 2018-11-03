13261534a8e359796b3c768316ef4add0172249d
Ian Greulich <ian@greulich.me>
2018-11-03 17:02:40 -0400

    add less to app for bundeling

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import '@babel/polyfill';
 import 'airbnb-browser-shims';
 
+import '../styles/app.less';
+
 const App = () => <div>Hello World!</div>;
 
 export default App;
